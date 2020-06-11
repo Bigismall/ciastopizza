b581ae46d82a0852869efaee41d0c3ae1bccbd6e
Bigismall <rondelious@gmail.com>
2020-06-11 18:18:28 +0200

    very first calculations

--- js/listener.js
+++ js/listener.js
@@ -1,3 +1,15 @@
-function helloPizza () { console.log("Hello Pizza!"); }
+function calculatePizza (type, amount) {
+  console.log(type, amount);
+  const hundredPercentFlour = 165;    //grams
+  const flour = Math.ceil(hundredPercentFlour * amount);
+  const water = Math.ceil(0.65 * flour);
+  const yeast = Math.ceil(0.01 * flour);
+  const sugar = Math.ceil(0.02 * flour);
+  const salt = Math.ceil(0.02 * flour);
+  const oil = Math.ceil(0.01 * flour);  //TODO in ml
 
-export default helloPizza;
+  return [flour, water, yeast, sugar, salt, oil];
+
+};
+
+export default calculatePizza;
